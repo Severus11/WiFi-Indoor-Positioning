d0806b1dc6e911a336c11fb8345810ccc7de0caf
Severus11 <36445722+Severus11@users.noreply.github.com>
2021-05-26 01:03:15 +0530

    Create classifier.h

---
+++ Classifier/classifier.h
@@ -0,0 +1,46 @@
+#pragma once
+namespace Eloquent {
+    namespace ML {
+        namespace Port {
+            class DecisionTree {
+                public:
+                    /**
+                    * Predict class for features vector
+                    */
+                    int predict(float *x) {
+                        if (x[2] <= 25.0) {
+                            if (x[4] <= 50.0) {
+                                return 1;
+                            }
+
+                            else {
+                                return 2;
+                            }
+                        }
+
+                        else {
+                            return 0;
+                        }
+                    }
+
+                    /**
+                    * Convert class idx to readable name
+                    */
+                    const char* predictLabel(float *x) {
+                        switch (predict(x)) {
+                            case 0:
+                            return "Bathroom";
+                            case 1:
+                            return "Bedroom";
+                            case 2:
+                            return "Kitchen";
+                            default:
+                            return "Houston we have a problem";
+                        }
+                    }
+
+                protected:
+                };
+            }
+        }
+    }
